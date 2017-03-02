6423cb4f1399b1cad2cfb05fda663900487b6f07
Nika Nevolko <nichechka@gmail.com>
2017-03-02 17:31:37 -0600

    modify hibernate installation

--- src/main/java/com/steeka/atmospot/persistence/PersistenceApplicationListener.java
+++ src/main/java/com/steeka/atmospot/persistence/PersistenceApplicationListener.java
@@ -1,18 +1,21 @@
 package com.steeka.atmospot.persistence;
 
 
+@WebListener
 public class PersistenceApplicationListener implements ServletContextListener {
-
 
     private EntityManagerFactory entityManagerFactory;
 
+    @Override
     public void contextInitialized(ServletContextEvent sce){
         ServletContext context = sce.getServletContext();
         entityManagerFactory = Persistence.createEntityManagerFactory("atmospot.persistence.manager1");
     }
 
+    @Override
     public void contextDestroyed(ServletContextEvent sce) {
         entityManagerFactory.close();
     }
+  
 }
 
